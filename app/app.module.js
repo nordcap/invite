0de787e004b9ccc176835363f885ce4cbae0496f
Alex Budaev <budaevaa@mail.ru>
2016-07-27 12:49:08 +0700

    Завершена основная разметка.

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,5 @@
 (function(){
   "use strict";
 
-  angular.module('inviteApp',['ngAnimate', 'ui.bootstrap','ngTouch','header', 'footer','inviteSystem']);
+  angular.module('inviteApp',['ngAnimate', 'ui.bootstrap','ngTouch','header', 'footer','inviteSystem', 'inviteTable']);
 })();
---
+++ app/invite-table/invite-table.component.js
@@ -0,0 +1,11 @@
+(function () {
+    "use strict";
+
+
+    angular.module('inviteTable').component('inviteTable', {
+        templateUrl: 'invite-table/invite-table.template.html',
+        controller: ['$http', function inviteTableController($http) {
+
+        }]
+    });
+})();
