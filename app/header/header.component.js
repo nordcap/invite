d28f608163554d47e7b6bfb0e4699d8e5e742e84
Alex Budaev <budaevaa@mail.ru>
2016-08-01 05:41:27 +0700

    Подгрузка данных из JSON файлов

--- app/header/header.component.js
+++ app/header/header.component.js
@@ -1,12 +1,17 @@
-(function(){
-  "use strict";
+(function () {
+    "use strict";
 
 
-  angular.module('header').
-  component('topMenu',{
-    templateUrl:'header/header.template.html',
-    controller: ['$http', function HeaderController($http){
-
-    }]
-  });
+    angular.module('header').component('topMenu', {
+        templateUrl: 'header/header.template.html',
+        controller: ['$http', function topMenuController($http) {
+            var self = this;
+            $http.get("server/people.json").then(
+                function success(response) {
+                    self.people =response.data;
+                }, function error(response) {
+                    console.log('error');
+                });
+        }]
+    });
 })();
--- app/invite-table/invite-table.component.js
+++ app/invite-table/invite-table.component.js
@@ -5,7 +5,13 @@
     angular.module('inviteTable').component('inviteTable', {
         templateUrl: 'invite-table/invite-table.template.html',
         controller: ['$http', function inviteTableController($http) {
-
+            var self = this;
+            $http.get("server/people-list.json").then(
+                function success(response) {
+                    self.people_list = response.data;
+                }, function error(response) {
+                    console.log('error');
+                });
         }]
     });
 })();
